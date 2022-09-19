f81872d78d04c4197b9af5b8c1705754f4148467
Lucas Oshiro <lucasseikioshiro@gmail.com>
2022-09-19 18:18:15 -0300

    round robin: add documentation for the new functions

--- round_robin.h
+++ round_robin.h
@@ -9,17 +9,16 @@
 
 #include "config.h"
 
-/* A round robin node, that represents a thread in a schueduler. The round
-   robin nodes holds a mutex and the thread_id of the previous and the next
-   thread.
+/* A round robin node, that represents a thread in a scheduler. The round robin
+   nodes holds a cond variable and its mutex, and the thread_id of the current,
+   the previous and the next thread.
 
    This way, this is kind of a linked list, but this does not hold a reference
    to the next node. This holds the thread_id of the next thread instead. The
    thread_id is enough to index another node.
 
-   The mutex is locked when it's the time for the thread represented by this
-   node to get a value from the queue. This can only be unlocked by the previous
-   node.
+   The conditional variable is used to wake a thread when it's its round. The
+   previous thread sends a signal to this, which will be unlocked.
 */
 typedef struct round_robin_node {
     int prev;
@@ -48,8 +47,14 @@ void init_round_robin_scheduler(round_robin_scheduler *rr);
 /* Add a subscriber to a scheduler. */
 void add_subscriber(round_robin_scheduler *rr, int thread_id);
 
+/* Remove a subscriber from a scheduler. */
 void remove_subscriber(round_robin_scheduler *rr, int thread_id);
 
+/* Changes the internal state of the schedule to the next subscriber. The thread
+   of the next subscriber will be signalized, and then it will proceed the
+   dequeue.
+*/
 int next_thread(round_robin_scheduler *rr);
 
+/* Wait until it's the round of thread with id thread_id. */
 void wait_round(round_robin_scheduler *rr, int thread_id);
